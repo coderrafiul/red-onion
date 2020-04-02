f8dcc10cac1db6480f7ab28da80c2c4404e3d868
coderrafiul <rafiul.rzb@gmail.com>
2020-04-02 22:20:15 +0600

    design corrected

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -4,7 +4,8 @@ import Banner from '../../images/bannerbackground.png';
 
 const Search = () => {
     return (
-        <div className="banner">
+        <div className="banner d-flex justify-content-center">
+          <h1>Best food waiting for your belly</h1>
           <form className="search">
           <input type="text" placeholder="Search food"/>
           <button className="banner-btn">Srarch</button>
